97987f02c01f63e2ca00a4ee3e71944fccbdf523
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-01-12 14:57:30 -0700

    Second change to testing.js
    This is a second change to testing.js

--- testing.js
+++ testing.js
@@ -1,5 +1,6 @@
 let x = 1;
-let y = 4;
-let z = x * y;
+let p = 4;
+let z = x * p;
 
 console.log("The value of z is: ", z);
+console.log("The chicken crossed the road and got to the other side....");
